d18d14beaa75f2ab0addb30ecbd87d301a495d15
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-03 10:10:34 +0200

    first commit

---
+++ TA20/src/test/java/com/TEAM4/UD20/TA20/AppTest.java
@@ -0,0 +1,20 @@
+package com.TEAM4.UD20.TA20;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
